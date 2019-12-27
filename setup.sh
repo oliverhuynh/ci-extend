92b18a259c4e84b567ec3017e48a33989e581ca4
Ky Huynh <ky.huynh@antbuddyc.com>
2019-12-27 09:53:54 +0630

    Cleaning up setup and more helpful tip

--- setup.sh
+++ setup.sh
@@ -1,42 +1,6 @@
 #!/bin/bash
 
-DEPLOYDIR=$1
-instance=$(basename `pwd`)
-DIR=$(dirname $0)
-cp $DIR/.gitlab-ci.yml ./.gitlab-ci.yml
-cp $DIR/.deploy ./
-DEPLOYDIR=$(echo $DEPLOYDIR | sed 's/\//\\\//g')
-
-INSTANCE=${2:-$instance}
-RELOADCOMMAND=${3:-'pm2 delete \\"'$INSTANCE'\\" || pm2 list; cd '$DEPLOYDIR' && pm2 start npm --name \\"'$INSTANCE'\\" -- start || pm2 list'}
-RELOADCOMMAND=$(echo $RELOADCOMMAND | sed 's/\//\\\//g' | sed 's/\&/\\\&/g')
-echo "RELOADCOMMAND: $RELOADCOMMAND"
-
-BUILDSCRIPT=${4:-'npm install;'}
-BUILDSCRIPT=$(echo $BUILDSCRIPT | sed 's/\//\\\//g' | sed 's/\&/\\\&/g')
-echo "BUILDSCRIPT: $BUILDSCRIPT"
-
-NODEVERSION=${5:-'10.17.0'}
-NODEVERSION=$(echo $NODEVERSION | sed 's/\//\\\//g' | sed 's/\&/\\\&/g')
-echo "NODEVERSION: $NODEVERSION"
-
-DOCKERNODE=${6:-'node:10.17.0-jessie'}
-DOCKERNODE=$(echo $DOCKERNODE | sed 's/\//\\\//g' | sed 's/\&/\\\&/g')
-echo "DOCKERNODE: $DOCKERNODE"
-
-sed -i 's/RELOADCOMMANDTOKEN/'"$RELOADCOMMAND"'/g' .gitlab-ci.yml
-sed -i 's/INSTANCETOKEN/'"$INSTANCE"'/g' .gitlab-ci.yml
-sed -i 's/DEPLOYDIRTOKEN/'"$DEPLOYDIR"'/g' .gitlab-ci.yml
-sed -i 's/BUILDSCRIPTTOKEN/'"$BUILDSCRIPT"'/g' .gitlab-ci.yml
-sed -i 's/NODEVERSIONTOKEN/'"$NODEVERSION"'/g' .gitlab-ci.yml
-sed -i 's/DOCKERNODETOKEN/'"$DOCKERNODE"'/g' .gitlab-ci.yml
-
-
-sed -i 's/RELOADCOMMANDTOKEN/'"$RELOADCOMMAND"'/g' .deploy
-sed -i 's/INSTANCETOKEN/'"$INSTANCE"'/g' .deploy
-sed -i 's/DEPLOYDIRTOKEN/'"$DEPLOYDIR"'/g' .deploy
-sed -i 's/BUILDSCRIPTTOKEN/'"$BUILDSCRIPT"'/g' .deploy
-sed -i 's/NODEVERSIONTOKEN/'"$NODEVERSION"'/g' .deploy
-sed -i 's/DOCKERNODETOKEN/'"$DOCKERNODE"'/g' .deploy
-
-echo "Remember to add SSH_PRIVATE_KEY + SSH_HOST_CONFIG (ssh-config options)"
+curdir=$(pwd)
+d=$(dirname $0)
+cd $d
+./gitlab-ci-extend --setup "$curdir" "$@"
